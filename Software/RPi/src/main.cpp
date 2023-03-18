a1ea47fbeb785ab1187dd08222743fec4aa965e5
Aruetiise <julius@becker-julius.de>
2023-03-18 19:15:52 +0100

    added main program for testing

---
+++ Software/RPi/src/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Julius on 18.03.2023.
+//
+
+#include "Models/HeatingTimeslot.h"
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    std::cout << "Hello, world!" << std::endl;
+    HeatingTimeslot::HeatingTimeslot slot;
+    std::cout << "Duration" << slot.getDuration() << std::endl;
+    return 0;
+}
