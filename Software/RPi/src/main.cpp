113675ffbf4d6d42e4deb49e27caa458051d3a0e
Aruetiise <julius@becker-julius.de>
2023-03-21 00:55:17 +0100

    trying wiringpi

--- Software/RPi/src/main.cpp
+++ Software/RPi/src/main.cpp
@@ -4,35 +4,17 @@
 
 #include "Models/HeatingTimeslot.h"
 #include <iostream>
-#include "../AdafruitDisplay/Adafruit_ST7735.h"
-
-// Pin definitions for your setup
-#define TFT_CS   24
-#define TFT_RST  18
-#define TFT_DC   16
-
-// Create display object
-Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
-
-void setup() {
-    // Initialize display
-    tft.initR(INITR_BLACKTAB);
-    // Set rotation
-    tft.setRotation(1);
-}
-
-void loop() {
-    // Set line color to white
-    uint16_t color = ST7735_WHITE;
-    // Draw line from (10,10) to (50,50)
-    tft.drawLine(10, 10, 50, 50, color);
-}
+#include <wiringPi.h>
+#include <wiringPiSPI.h>
 
 int main(int argc, char* argv[]) {
     std::cout << "Hello, world!" << std::endl;
     HeatingTimeslot slot;
     std::cout << "Duration " << slot.getDuration().getHour() << ":" << slot.getDuration().getMinute() << std::endl;
-    setup();
-    loop();
+
+    wiringPiSPISetup(0, 1000000);  // Configure the SPI interface with a clock speed of 1MHz
+
+    unsigned char data[] = {0x01, 0x02, 0x03, 0x04};
+    wiringPiSPIDataRW(0, data, sizeof(data));  // Write the data to the device
     return 0;
 }
