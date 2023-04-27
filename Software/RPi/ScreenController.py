29b7a35e6fd017573b98b157b593618c23c44813
Aruetiise <julius@becker-julius.de>
2023-04-27 16:01:15 +0200

    test Screen

--- Software/RPi/ScreenController.py
+++ Software/RPi/ScreenController.py
@@ -1,21 +1,18 @@
-import time
-import sys
 from luma.core.interface.serial import spi
 from luma.lcd.device import st7735
 from luma.core.render import canvas
 
 class ScreenController:
     def __init__(self):
-        self.screen = None
-        self.s = spi(port=0, device=0, cs_high=True, gpio_DC=23, gpio_RST=24)
-        self.device=st7735(self.s,rotate=0,width=160,height=128,h_offset=0,v_offset=0,bgr=False)
+        s = spi(port=0, device=0, cs_high=True, gpio_DC=23, gpio_RST=24)
+        self.device=st7735(s,rotate=0,width=160,height=128,h_offset=0,v_offset=0,bgr=False)
         self.device.backlight(False)
-        with canvas(self.device) as draw:
-            self.draw = draw
+        #with canvas(self.device) as draw:
+        #    self.draw = draw
     
     def showBaseStats(self):
-        self.draw.rectangle((4, 4, 80, 124), outline='blue', fill=(22, 55, 55))
-        time.sleep(1)
+        with canvas(self.device) as draw:
+            draw.rectangle((4, 4, 80, 124), outline='blue', fill=(22, 55, 55))
     
     def showMenu(self):
         pass
